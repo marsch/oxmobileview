8579dd3b56fd4064e94ee8abd3058f9edb02bc3c
Mario Scheliga <marsch.js@gmail.com>
2013-09-14 05:04:47 +0200

    WIP - first commit with grunt build system, not perfect

---
+++ src/js/main.build.js
@@ -0,0 +1,37 @@
+
+require.config({
+    baseUrl: './js',
+    paths: {
+        text: 'vendor/requirehogan/text',
+        hgn: 'vendor/requirehogan/hgn',
+        cs: 'vendor/requirecs/cs',
+        css: 'vendor/requirecss/css',
+        less: 'vendor/requireless/less',
+        lessc: 'vendor/requireless/lessc',
+        'lessc-server': 'vendor/requireless/lessc-server',
+        'less-builder': 'vendor/requireless/less-builder',
+        'css/css-builder': 'vendor/requirecss/css-builder',
+        'css/normalize': 'vendor/requirecss/normalize',
+        normalize: 'vendor/requirecss/normalize',
+        'coffee-script': 'vendor/requirecs/coffee-script',
+        hogan: 'vendor/requirehogan/hogan',
+        loglevel: 'vendor/loglevel/loglevel',
+
+
+    },
+    shim: {
+    }
+});
+
+require([
+    'less!../css/main',
+    'loglevel'
+    ], function (Application, log) {
+  'use strict';
+
+  log.debug('app started');
+
+});
+
+
+
